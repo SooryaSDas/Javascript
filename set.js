4e0e008cf4bc2e58254ed684c3f823daba4fec95
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-03-30 15:32:58 +0530

    set in javascript

---
+++ set.js
@@ -0,0 +1,21 @@
+// Create a Set
+const letters = new Set(["a","b","c"]);
+
+// List all Elements
+let text = "";
+letters.forEach (function(value) {
+  text += value;
+})
+
+// Create a Set
+const letter = new Set();
+
+// Add Values to the Set
+letter.add("a");
+letter.add("b");
+letter.add("c");
+
+// Add Variables to the Set
+letters.add(a);
+letters.add(b);
+letters.add(c);
