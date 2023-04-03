d666fdc36b2205c87e5199066c4d3d9529dc26dd
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-03 22:01:19 +0530

    Date in Javascript

---
+++ ADVANCED JAVASCRIPT CONCEPTS/date.js
@@ -0,0 +1,8 @@
+const dt = new Date();
+console.log(dt); // 2023-04-03T16:18:36.067Z
+
+const d = new Date("2023-04-03");
+console.log(d); // 2023-04-03T00:00:00.000Z
+
+const dte = new Date();
+dte.toDateString();
