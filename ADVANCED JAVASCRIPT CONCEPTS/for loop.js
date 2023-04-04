c3b34513be7b9515340e625612c8a80f08129572
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-04 15:13:11 +0530

    for loop in javascript

---
+++ ADVANCED JAVASCRIPT CONCEPTS/for loop.js
@@ -0,0 +1,16 @@
+const arr = [10,20,30,40,50,60,70,80,90,100];
+let length = arr.length;
+for(let i=0;i<length;i++){
+    console.log(arr[i]);
+}
+// output
+// 10
+// 20
+// 30
+// 40
+// 50
+// 60
+// 70
+// 80
+// 90
+// 100
