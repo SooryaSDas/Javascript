c1aa7ece00eceee9a0bccf5a8e6c1f14d0fc6616
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-08 09:08:25 +0530

    operators in javascript

---
+++ operators.js
@@ -0,0 +1,32 @@
+// OPERATORS
+
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z:
+let z = x + y;
+
+// MULTIPLICATON
+let x = 5;
+let y = 2;
+let z = x * y;
+
+//comparison
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+console.log(result); // true
+
+let text1 = "What a very ";
+text1 += "nice day"; // What a very nice day
+
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+// 10
+// 55
+// Hello5
+
+
+
