e931220e777edbc0930ff498d2cf7b98e707881f
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-08 09:07:10 +0530

    whileloop in javascript

---
+++ ADVANCED JAVASCRIPT CONCEPTS/whileloop.js
@@ -0,0 +1,19 @@
+// WHILE LOOP
+
+let txt = "";
+let i = 1;
+while(i<10){
+    txt += "The number is " + i + "\n";
+    i++;
+}
+console.log(txt);
+
+// The number is 1
+// The number is 2
+// The number is 3
+// The number is 4
+// The number is 5
+// The number is 6
+// The number is 7
+// The number is 8
+// The number is 9
