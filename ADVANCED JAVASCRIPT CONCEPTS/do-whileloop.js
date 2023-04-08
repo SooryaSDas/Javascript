e9a35e481fe31b34ae4ecd8b72fb05b4cd67c305
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-08 09:07:57 +0530

    do-whileloop in javascript

---
+++ ADVANCED JAVASCRIPT CONCEPTS/do-whileloop.js
@@ -0,0 +1,22 @@
+// DO-WHILE LOOP
+let text = ""
+let i = 0;
+
+do {
+  text += "The number is " + i + "\n";
+  i++;
+}
+while (i < 10); 
+console.log(text);
+
+// output
+// The number is 0
+// The number is 1
+// The number is 2
+// The number is 3
+// The number is 4
+// The number is 5
+// The number is 6
+// The number is 7
+// The number is 8
+// The number is 9
