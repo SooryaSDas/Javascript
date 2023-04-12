12d7801a098d65acc9fc698dfaab2161da3fba24
SOORYA S DAS <83228601+SooryaSDas@users.noreply.github.com>
2023-04-12 21:31:02 +0530

    number reversing

--- ADVANCED JAVASCRIPT CONCEPTS/num_reverse.js
+++ ADVANCED JAVASCRIPT CONCEPTS/num_reverse.js
@@ -6,3 +6,16 @@ function reverse_num(n){
     return num;
 }
 console.log(reverse_num(12345));
+
+// reversing a number without inbuilt function
+function reverse_number(num){
+    let n = num.toString();
+    const number = [];
+    let str = "";
+    for(let i = n.length-1,j=0;i>=0;i--,j++){
+        number[j] = n[i];
+        str = str+number[j];
+    }
+    return parseInt(str);
+}
+console.log(reverse_number(12345));
